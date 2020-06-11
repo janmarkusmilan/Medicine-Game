d25a3f4c2ccc48bf1b2bb5aebac3edc99992b062
Jan Markus Milan <56745538+janmarkusmilan@users.noreply.github.com>
2020-06-11 01:03:37 -0400

    Add files via upload

---
+++ Scripts/In–Game/BlinkEffect.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BlinkEffect : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> highlights1;
+
+    [SerializeField]
+    private List<GameObject> highlights2;
+
+    public void FirstAreaBlink()
+    {
+        for (int i = 0; i < highlights1.Count; i++)
+            highlights1[i].SetActive(true);
+
+        StartCoroutine(Blink1(500.0f)); // 500 seconds
+    }
+
+    IEnumerator Blink1(float waitTime)
+    {
+        var endTime = Time.time + waitTime;
+        while (Time.time < endTime)
+        {
+            GetComponent<Renderer>().enabled = false;
+            yield return new WaitForSeconds(0.5f); // Render on after 0.5 seconds
+            GetComponent<Renderer>().enabled = true;
+            yield return new WaitForSeconds(0.5f); // Render off after 0.5 seconds
+        }
+    }
+
+    public void SecondAreaBlink()
+    {
+        for (int i = 0; i < highlights2.Count; i++)
+            highlights2[i].SetActive(true);
+
+        StartCoroutine(Blink2(500.0f)); // 500 seconds
+    }
+
+    IEnumerator Blink2(float waitTime)
+    {
+        var endTime = Time.time + waitTime;
+        while (Time.time < endTime)
+        {
+            GetComponent<Renderer>().enabled = false;
+            yield return new WaitForSeconds(0.5f); // Render on after 0.5 seconds
+            GetComponent<Renderer>().enabled = true;
+            yield return new WaitForSeconds(0.5f); // Render off after 0.5 seconds
+        }
+    }
+}
---
+++ Scripts/In–Game/FadeBetweenLevel.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FadeBetweenLevel : MonoBehaviour
+{
+    public Transform player;
+
+    public Image fadeImage;
+    public GameObject fadeCanvas;
+    public GameObject RPGTalkBegin;
+
+    public AudioSource backgroundMusic;
+    public float duration = 5f;
+    public float targetVolume = 0f;
+
+    public string sceneName;
+
+    IEnumerator Start()
+    {
+        player.GetComponent<DialogueAndMovement>().CancelControls();
+        fadeImage.canvasRenderer.SetAlpha(1.0f);
+
+        fadeImage.CrossFadeAlpha(0.0f, 3.5f, false);
+        yield return new WaitForSeconds(3.5f);
+        FadeUnactiveAndBeginDialogue();
+    }
+
+    public void FadeUnactiveAndBeginDialogue()
+    {
+        fadeCanvas.SetActive(false);
+        RPGTalkBegin.SetActive(true);
+        player.GetComponent<DialogueAndMovement>().GiveBackControls();
+        GameObject.Find("[LPC] Cabinet (2)").GetComponent<Interactable>().enabled = false;
+    }
+
+    public void StartFadeOut()
+    {
+        StartCoroutine(FadeOutToNextScene());
+    }
+
+    IEnumerator FadeOutToNextScene()
+    {
+        fadeCanvas.SetActive(true);
+        fadeImage.canvasRenderer.SetAlpha(0.0f);
+        fadeImage.CrossFadeAlpha(1.0f, 5.0f, false);
+        yield return new WaitForSeconds(5.0f);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void FadeOutMusic()
+    {
+        backgroundMusic = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+        StartCoroutine(FadeMusic.StartFade(backgroundMusic, duration, targetVolume));
+    }
+}
